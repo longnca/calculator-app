8a1551607244874027bdf36343fae96170e9516f
longbytes <dsbuff01@gmail.com>
2024-04-29 21:14:11 -0600

    fix operators error

--- js/script.js
+++ js/script.js
@@ -2,19 +2,32 @@
 
 const display = document.getElementById("display")
 
-function appendToDisplay(input){
+function appendToDisplay(input) {
+    if (input === 'x') input = '*';
+
+    if (isOperator(input) && isOperator(lastCharacterInDisplay())) {
+        return; // Prevent consecutive operators
+    }
     display.value += input;
 }
 
-function clearDisplay(){
+function clearDisplay() {
     display.value = "";
 }
 
-function calculate(){
+function calculate() {
     try{
-        display.value = eval(display.value);
+        display.value = math.evaluate(display.value);
     }
     catch(error){
         display.value = "Error";
     }
+}
+
+function isOperator(char) {
+    return ['+', '-', 'x', '/'].includes(char);
+}
+
+function lastCharacterInDisplay() {
+    return display.value.slice(-1);
 }
